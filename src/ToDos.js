27e1d65d77c84662d2372bbfa74ac4e1034448e7
aspittel <alison.spittel@generalassemb.ly>
2020-07-13 13:59:30 -0500

    update hooks

--- src/ToDos.js
+++ src/ToDos.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import ToDo from './ToDo'
 import './App.css'
 
-class ToDos {
-  constructor() {
+function ToDos () {
+  useStte({
+    todos: [{
+      task: "Feed Blair",
+      done: false
+    }, {
+      task: "clean apartment",
+      done: false
+    } {
+      task: "create debugging assignment",
+      done: true
+    }],
+    newTodo: ''
+  })
 
-    this.stte = {
-      todos: [{
-        task: "Feed Blair",
-        done: false
-      }, {
-        task: "clean apartment",
-        done: false
-      } {
-        task: "create debugging assignment",
-        done: true
-      }],
-      newTodo: ''
-    }
-  }
-
-  handleDone(e, idx) {
-    this.setState(prevState => {
+  const handleDone = (e, idx) => {
+    updateTodos(prevState => {
       prevState.toos[idx].done = !prevState.todos[idx]done
       return prevState
     })
   }
 
-  handleChange () {
-    this.setState({
-      newTodo: e.target
-    })
+  const handleChange = () => {
+    updateTodos(
+      e.target
+    )
   }
 
 
-  addTodo (e) {
+  const addTodo = (e) => {
     e.preventDefault()
 
-    this.setState(prevState => {
+    updateTodos(prevState => {
       prevState.todo.push({
         task: this.state.newTodo,
         done: false
@@ -47,31 +44,29 @@ class ToDos {
     })
   }
 
-  render() {
-    return (
-        <h1>Things to do today...</h1>
-        <form onSubmit={this.addTodo}>
-          <label>
-            <input
-              type="text"
-              placeholder="What do you need to do today?"
-              value={this.state.todo}
-              onChange={this.handleChange}
-            />
-          </label>
-        </form>
-        <div class="todo-container">
-          {this.state.todos.map((todo, idx) =>
-            <ToDo
-              ask={todo.task}
-              todo={todo.done}
-              handleDone={e => this.handleDone(e, idx)}
-              key={idx}
-            />
-          )}
-        </div>
-    )
-  }
+  return (
+      <h1>Things to do today...</h1>
+      <form onSubmit={this.addTodo}>
+        <label>
+          <input
+            type="text"
+            placeholder="What do you need to do today?"
+            value={this.state.todo}
+            onChange={this.handleChange}
+          />
+        </label>
+      </form>
+      <div class="todo-container">
+        {todos.map((todo, idx) =>
+          <ToDo
+            ask={todo.task}
+            todo={todo.done}
+            handleDone={e => this.handleDone(e, idx)}
+            key={idx}
+          />
+        )}
+      </div>
+  )
 }
 
 export default ToDos
